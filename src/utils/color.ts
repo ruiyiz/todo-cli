ea04c983ff6b4bf626e34672f6dd7a6499dffb58
Ruiyi Zhang <ruiyizhang@gmail.com>
2026-02-09 09:19:25 -0500

    Fix chalk v5 compatibility: use Chalk class instead of removed chalk.Instance

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,9 +1,9 @@
-import chalk, { type ChalkInstance } from "chalk";
+import chalk, { Chalk, type ChalkInstance } from "chalk";
 
 let _chalk: ChalkInstance = chalk;
 
 export function setColorEnabled(enabled: boolean): void {
-  _chalk = new chalk.Instance({ level: enabled ? chalk.level : 0 });
+  _chalk = new Chalk({ level: enabled ? chalk.level : 0 });
 }
 
 export function c(): ChalkInstance {
